92fea38ef919fe133a2d21751e56a45da7e0bff1
jimlindybner <lin.jycl@gmail.com>
2023-02-23 13:08:17 -0500

    attempted j3 - able to add vowel and next consonant. original vowel missing.

--- HTTP5112Assignment2/Controllers/HTTP5112Assignment2Controller.cs
+++ HTTP5112Assignment2/Controllers/HTTP5112Assignment2Controller.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Security.Principal;
 using System.Web.Http;
 
 namespace HTTP5112Assignment2.Controllers
@@ -116,6 +118,82 @@ namespace HTTP5112Assignment2.Controllers
                 //plural string output
                 return PluralOpening + Solution + PluralClosing;
             }
+        }
+
+        /// <summary>
+        /// A program that translates English words (lower case input) into Rovarspraket.
+        /// </summary>
+        /// <param name="Word"></param>
+        /// <returns>
+        /// Every consonant in the input word is replaced with three letters:
+        /// 1) the consonant itself
+        /// 2) the vowel closest to it in the alphabet
+        /// 3) the next consonant in the alphabet
+        /// </returns>
+        /// <example>
+        /// GET ../api/J3/Rovarspraket/joy -> jikoyuz
+        /// GET ../api/J3/Rovarspraket/ham -> hijamon
+        /// </example>
+
+        //GET: /api/J3/Rovarspraket/{Word}
+        [HttpGet]
+        [Route("api/J3/Rovarspraket/{Word}")]
+        public string Rovarspraket(string Word)
+        {
+            string[] Alphabet = {
+                "a","b","c",
+                "d","e","f","g",
+                "h","i","j","k","l",
+                "m","n","o","p","q","r",
+                "s","t","u","v","w","x","y","z"
+            };
+            string[] Vowels = { "a", "e", "i", "o", "u" };
+            char[] Consonants = {'b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'};
+            string Letter;
+            string Consonant;
+            string AddVowel; //add closest vowel
+            string ConsFin; //add next consonant
+
+            //gather letters in {Word} in an array
+            char[] Letters = Word.ToCharArray();
+
+            //loop for extracting individual letters
+            for (int i = 0; i < Letters.Length; i++)
+            {
+                //loop looking for consonants
+                for (int j = 0; j < Consonants.Length; j++)
+                {
+                    //extract individual letters
+                    Letter = Letters[i].ToString();
+                    //identify the consonants and modify
+                    if (Letter.Contains(Consonants[j]))
+                    {
+                        //identify consonants
+                        Consonant = Letter;
+                        //add closest vowel to consonant
+                        if (Consonant == "b" || Consonant == "c")
+                        {
+                            AddVowel = Consonant + "a";
+                        } else if (Consonant == "d" || Consonant == "f" || Consonant == "g")
+                        {
+                            AddVowel = Consonant + "e";
+                        } else if (Consonant == "h" || Consonant == "j" || Consonant == "k" || Consonant == "l")
+                        {
+                            AddVowel = Consonant + "i";
+                        } else if (Consonant == "m" || Consonant == "n" || Consonant == "p" || Consonant == "q" || Consonant == "r")
+                        {
+                            AddVowel = Consonant + "o";
+                        } else
+                        {
+                            AddVowel = Consonant + "u";
+                        }
+                        //add next consonant
+                        ConsFin = AddVowel + Consonants[j + 1];
+                        Console.Write(ConsFin);
+                    }
+                }
+            }
+
         }
     }
 }
